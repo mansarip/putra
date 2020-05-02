3bed66ded50141fac5bd9b462e66f3a10baf5763
Luqman <luqmanrasa@gmail.com>
2020-05-03 01:00:33 +0800

    udpate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Pane, Text, Heading } from "evergreen-ui";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Pane
+        display="grid"
+        gridTemplateColumns="1fr 1fr"
+        columnGap={10}
+        paddingY={20}
+        paddingX={15}
+        borderBottom="0px solid #ececec"
+        boxShadow="0px 1px 5px 1px #ececec"
+        background="#fff"
+        userSelect="none"
+      >
+        <Heading
+          userSelect="none"
+          fontWeight="bold"
+          color="#333"
+          fontFamily="inherit"
+          fontSize={20}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Putra.me
+          <br />
+          <Text>Simple ID Creation</Text>
+        </Heading>
+
+        <Pane
+          display="flex"
+          justifyContent="flex-end"
+          alignItems="center"
+          userSelect="none"
+          className="tap"
+        >
+          <img
+            src="/images/interface.svg"
+            alt=""
+            style={{ width: 25, marginRight: 10 }}
+          />
+          <Text color="#333" fontWeight="bold">
+            Lang (EN)
+          </Text>
+        </Pane>
+      </Pane>
+
+      <Pane
+        display="grid"
+        gridTemplateColumns="1fr 1fr"
+        columnGap={10}
+        padding={15}
+      >
+        <MainButton icon="qr-code.svg" label="Generate QR" />
+        <MainButton icon="business.svg" label="Scanner" />
+        <MainButton icon="team.svg" label="Host" />
+        <MainButton icon="document.svg" label="Others" />
+      </Pane>
+
+      {/* <Pane paddingX={15}>
+        <Heading
+          userSelect="none"
+          fontWeight="bold"
+          color="#333"
+          fontFamily="inherit"
+          fontSize={16}
+        >
+          Guides
+        </Heading>
+      </Pane> */}
+    </>
   );
 }
 
-export default App;
+function MainButton({ label, icon }) {
+  return (
+    <Pane
+      background="#fff"
+      padding={10}
+      boxShadow="0px 1px 1px 1px #ececec"
+      borderRadius={5}
+      userSelect="none"
+      textAlign="center"
+      marginBottom={10}
+      className="tap"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+    >
+      <Pane width={50} marginY={10}>
+        <img src={`/images/${icon}`} alt="" style={{ width: "100%" }} />
+      </Pane>
+      <Text fontSize={16} fontWeight="bold" color="#333">
+        {label}
+      </Text>
+    </Pane>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ThemeProvider, defaultTheme } from "evergreen-ui";
+
+/**
+ * override default font
+ */
+const myTheme = { ...defaultTheme };
+myTheme.typography.fontFamilies.display = "Jost";
+myTheme.typography.fontFamilies.ui = "Jost";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider value={myTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
