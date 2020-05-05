da50f8fb1ea487978307918d14ee9e8657523dab
Luqman <luqmanrasa@gmail.com>
2020-05-05 10:46:02 +0800

    export csv

--- src/PageLogDetails.js
+++ src/PageLogDetails.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Topbar from "./Topbar";
-import { Icon, Text, Pane, Paragraph } from "evergreen-ui";
+import { Position, Icon, Text, Pane, Paragraph, Popover } from "evergreen-ui";
 import { useHistory } from "react-router-dom";
 import db from "./db";
 import AutoSizer from "react-virtualized-auto-sizer";
 import { FixedSizeList } from "react-window";
 import moment from "moment/moment";
 import capitalize from "capitalize";
+import { unparse } from "papaparse";
 
 export default function PageLogDetails() {
   const history = useHistory();
@@ -20,6 +21,55 @@ export default function PageLogDetails() {
 
     setList(result);
     setIsLoading(false);
+  }
+
+  function saveAsCSV() {
+    const data = [];
+    const header = [
+      "NAME",
+      "PHONE NUMBER",
+      "DATETIME",
+      "DATE",
+      "YEAR",
+      "MONTH",
+      "DAY",
+      "TIME",
+      "HOUR",
+      "MINUTE",
+    ];
+
+    for (const record of list) {
+      data.push([
+        record.name,
+        record.phone,
+        moment(record.date + record.time, "YYYYMMDDHHmm").format(
+          "DD-MM-YYYY HH:mm A"
+        ),
+        record.date,
+        record.year,
+        record.month,
+        record.day,
+        record.time,
+        record.hour,
+        record.minute,
+      ]);
+    }
+
+    const csv = unparse(
+      {
+        fields: header,
+        data: data,
+      },
+      {
+        quotes: true,
+      }
+    );
+
+    const hiddenElement = document.createElement("a");
+    hiddenElement.href = "data:text/csv;charset=utf-8," + encodeURI(csv);
+    hiddenElement.target = "_blank";
+    hiddenElement.download = `putra_log_${date}.csv`;
+    hiddenElement.click();
   }
 
   useEffect(() => {
@@ -37,14 +87,46 @@ export default function PageLogDetails() {
           </Text>
         }
         rightElement={
-          <Pane
-            display="flex"
-            alignItems="center"
-            justifyContent="flex-end"
-            padding={15}
+          <Popover
+            position={Position.BOTTOM}
+            content={({ close }) => (
+              <Pane
+                width={300}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                flexDirection="column"
+                background="#ececec"
+              >
+                <MenuOption
+                  icon="panel-table"
+                  title="Download as .CSV"
+                  description="Save to your device as .csv file. This file can be opened with MS Excel or Google Sheet."
+                  color="green"
+                  onClick={() => {
+                    close();
+                    saveAsCSV();
+                  }}
+                />
+                <MenuOption
+                  icon="trash"
+                  title="Delete Log"
+                  description="Permanently remove this log. Note that this action cannot be undone."
+                  color="#cc0606"
+                />
+              </Pane>
+            )}
           >
-            <Icon icon="search" size={15} />
-          </Pane>
+            <Pane
+              display="flex"
+              alignItems="center"
+              justifyContent="flex-end"
+              padding={15}
+              paddingRight={0}
+            >
+              <Icon icon="more" size={15} />
+            </Pane>
+          </Popover>
         }
       />
 
@@ -142,5 +224,31 @@ export default function PageLogDetails() {
         )}
       </Pane>
     </>
+  );
+}
+
+function MenuOption({ title, description, color, icon, onClick }) {
+  return (
+    <Pane
+      display="grid"
+      gridTemplateColumns="50px 1fr"
+      padding={10}
+      className="tap"
+      marginBottom={1}
+      background="#fff"
+      userSelect="none"
+      onClick={onClick || null}
+    >
+      <Pane textAlign="center" paddingTop={3}>
+        <Icon icon={icon} size={20} color={color} />
+      </Pane>
+      <Pane lineHeight={1.2}>
+        <Text color="#333" fontWeight="bold" fontSize={15}>
+          {title}
+        </Text>
+        <br />
+        <Text>{description}</Text>
+      </Pane>
+    </Pane>
   );
 }
